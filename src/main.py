d31745ce4a6dc345aa34858d748657aa7e10095d
Abhishek Kumar <abhishek1py@gmail.com>
2025-05-15 08:07:06 +0530

    Day-1 completed

--- src/main.py
+++ src/main.py
@@ -1,24 +1,138 @@
+### ======================================== DAY - 1 ========================================
 from fastapi import FastAPI
-from products import products
-import uvicorn
+from enum import Enum
+from .products import products
 
 app = FastAPI()
-# uvicorn main: app - -reload
-# uvicorn main: app - -host 0.0.0.0 - -port 80 - -reload
+
+class Category(str, Enum):
+    MOBILE = "Mobile"
+    FASHION = "Fashion"
+    HOME = "Home"
+    ELECTRONICS = "Electronics"
+
 
 @app.get('/')
 async def home():
-    return {"message": "Welcome to FastAPI tutorial!"}
+    return {"message": "Welcome to FastAPI day-1."}
 
+## ------------------ Topic :: Order Matters ------------------
+# Wrong order.
+# @app.get('/greet/{name}')
+# async def greet_user(name):
+#     return {'message': f'Good morning! {name}.'}
 
-# @app.get('/product')
-# async def product_list():
-#     return products
+# @app.get('/greet/welcome')
+# async def welcome():
+#     return {"message", "This will welcome message."}
     
+## Correct Order
+@app.get('/greet/welcome')
+async def welcome():
+    return {"message": "This is welcome message."}
+    
+## ------------------ Topic :: Path Parameters ------------------
+@app.get('/greet/{name}')
+async def greet_user(name):
+    return {'message': f'Good morning! {name}.'}
 
-@app.get("/product/{product_id}")
-async def product_view(product_id:int):
+
+## ------------------ Topic :: Type Conversion ------------------
+@app.get('/item/{price}')
+async def product(price:float):
+    print(f"Price : {price} & Type : {type(price)}")
+    return {"Item Price":price, "message":"Price is in float."}
+
+
+## ------------------ Topic :: Multiple Path Parameters ---------
+@app.get('/user/{user_id}/post/{post_id}')
+async def get_post_by_user(user_id:int, post_id:int):
+    return {"user_id":user_id, "post_id":post_id}
+
+
+## ------------------ Topic :: Path Converter ---------
+# @app.get('/files/{file_path}') # dev/test/main.py - Error: Not Found
+# or : dev\test\main.py - "file_path": "dev\\test\\main.py", 
+@app.get('/files/{file_path:path}') # dev\test\main.py -- "dev\\test\\main.py"
+async def read_file(file_path:str):
+    return {"file_path":file_path, "message":"File is read completely."}
+
+## ------------------ Topic :: Using Enum ---------
+@app.get('/product/{category}')
+async def product_by_category(category:Category):
+    filtered_product = []
+    # print(f"Name : {category.name} & value : {category.value}")
     for prod in products:
-        if prod.get('id') == product_id:
-            return {"message":"Product Available", "product":prod}
-    return {"message":"Product Not Found"}
+        if prod.get('category') == category.value:
+            filtered_product.append(prod)
+
+    return {"products": filtered_product}
+### ======================================== DAY - 1 ========================================
+from fastapi import FastAPI
+from enum import Enum
+from .products import products
+
+app = FastAPI()
+
+class Category(str, Enum):
+    MOBILE = "Mobile"
+    FASHION = "Fashion"
+    HOME = "Home"
+    ELECTRONICS = "Electronics"
+
+
+@app.get('/')
+async def home():
+    return {"message": "Welcome to FastAPI day-1."}
+
+## ------------------ Topic :: Order Matters ------------------
+# Wrong order.
+# @app.get('/greet/{name}')
+# async def greet_user(name):
+#     return {'message': f'Good morning! {name}.'}
+
+# @app.get('/greet/welcome')
+# async def welcome():
+#     return {"message", "This will welcome message."}
+    
+## Correct Order
+@app.get('/greet/welcome')
+async def welcome():
+    return {"message": "This is welcome message."}
+    
+## ------------------ Topic :: Path Parameters ------------------
+@app.get('/greet/{name}')
+async def greet_user(name):
+    return {'message': f'Good morning! {name}.'}
+
+
+## ------------------ Topic :: Type Conversion ------------------
+@app.get('/item/{price}')
+async def product(price:float):
+    print(f"Price : {price} & Type : {type(price)}")
+    return {"Item Price":price, "message":"Price is in float."}
+
+
+## ------------------ Topic :: Multiple Path Parameters ---------
+@app.get('/user/{user_id}/post/{post_id}')
+async def get_post_by_user(user_id:int, post_id:int):
+    return {"user_id":user_id, "post_id":post_id}
+
+
+## ------------------ Topic :: Path Converter ---------
+# @app.get('/files/{file_path}') # dev/test/main.py - Error: Not Found
+# or : dev\test\main.py - "file_path": "dev\\test\\main.py", 
+@app.get('/files/{file_path:path}') # dev\test\main.py -- "dev\\test\\main.py"
+async def read_file(file_path:str):
+    return {"file_path":file_path, "message":"File is read completely."}
+
+## ------------------ Topic :: Using Enum ---------
+@app.get('/product/{category}')
+async def product_by_category(category:Category):
+    filtered_product = []
+    # print(f"Name : {category.name} & value : {category.value}")
+    for prod in products:
+        if prod.get('category') == category.value:
+            filtered_product.append(prod)
+
+    return {"products": filtered_product}
